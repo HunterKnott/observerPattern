085c06c52454f07d19636ac5049e23ffffdb20db
Hunter Knott <hunterknott00@gmail.com>
2024-02-10 11:47:23 -0700

    Removed unnecessary code and files

--- observerPattern/src/op/controller/Snapshot.java
+++ observerPattern/src/op/controller/Snapshot.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 
 public class Snapshot {
 	private String dateTime;
-//	private ArrayList<String> lines;
-	
-	// Try make this replace the lines list
 	private ArrayList<List<String>> data;
 	
 	public Snapshot(String dateTime, ArrayList<List<String>> data) {
@@ -21,22 +18,6 @@ public class Snapshot {
 	public void setDateTime(String dateTime) {
 		this.dateTime = dateTime;
 	}
-
-//	public List<String> getLines() {
-//		return lines;
-//	}
-//
-//	public void setLines(ArrayList<String> lines) {
-//		this.lines = lines;
-//	}
-//	
-//	public void printLines() {
-//		System.out.println(getDateTime());
-//		for (int i = 0; i < lines.size(); i++) {
-//			System.out.println(lines.get(i));
-//		}
-//		System.out.println("");
-//	}
 
 	public ArrayList<List<String>> getData() {
 		return data;
